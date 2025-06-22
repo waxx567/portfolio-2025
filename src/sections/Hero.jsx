1adc099bb1c8b24d2bf68d8defc9d76f7d06ce11
Wayne <waynem567@gmail.com>
2025-06-22 15:42:42 +0200

    Fix index.css and update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section id='hero' className='relative overflow-hidden'>
+      <div className='absolute top-0 left-0 z-10'>
+        <img src='../images/bg.png' alt='background' />
+      </div>
+
+      <div className='hero-layout'>
+        {/* LEFT: HERO CONTENT */}
+        {/* RIGHT: HERO CONTENT */}
+    </section>
   )
 }
 
