befe9fe7679e2da92e6b05409317917921ecfdf9
Wayne <waynem567@gmail.com>
2025-06-22 15:51:32 +0200

    Uodate Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,7 +10,13 @@ const Hero = () => {
       <div className='hero-layout'>
         {/* LEFT: HERO CONTENT */}
         <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
-            
+          <div className='flex flex-col gap-7'>
+            <div className='hero-text'>
+                <h1>Shaping</h1>
+                <h1>into Real Projects</h1>
+                <h1>that Deliver Results</h1>
+            </div>
+          </div>
         </header>
         {/* RIGHT: 3D MODEL */}
       </div>
