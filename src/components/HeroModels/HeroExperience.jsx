bc65f4bb99b171a5b255f5d6dcdc38159de3a474
Wayne <waynem567@gmail.com>
2025-06-24 09:30:45 +0200

    Update HeroExperience.jsx

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 const HeroExperience = () => {
+  const isTablet = false
+
   return (
-    <div>HeroExperience</div>
+    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+      <ambientLight intensity={0.2} color='#1A1A40' />
+      <directionalLight position={[5, 5, 5]} intensity={5} />
+
+      <OrbitControls 
+        enablePan={false}
+        enableZoom={!isTablet}
+        maxDistance={20}
+        minDistance={5}
+        minPolarAngle={Math.PI / 5}
+        maxPolarAngle={Math.PI / 2}
+      />
+      
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color='teal' />
+      </mesh>
+    </Canvas>
   )
 }
 
