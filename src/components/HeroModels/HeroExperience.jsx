2fa369a4d5dfbd21270393060c8811ac0e79c90d
Wayne <waynem567@gmail.com>
2025-06-24 10:41:42 +0200

    Change directionalLight intensity

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -10,7 +10,7 @@ const HeroExperience = () => {
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <ambientLight intensity={0.2} color='#1A1A40' />
-      <directionalLight position={[5, 5, 5]} intensity={5} />
+      <directionalLight position={[5, 5, 5]} intensity={2} />
 
       <OrbitControls 
         enablePan={false}
