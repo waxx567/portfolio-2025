d13611683e3ceb4f9edb5889dc5820645fe5cab4
Wayne <waynem567@gmail.com>
2025-06-24 09:26:38 +0200

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -52,24 +52,7 @@ const Hero = () => {
 
         {/* RIGHT: 3D MODEL */}
         <figure>
-          <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-            <ambientLight intensity={0.2} color='#1A1A40' />
-            <directionalLight position={[5, 5, 5]} intensity={5} />
-
-            <OrbitControls 
-              enablePan={false}
-              enableZoom={!isTablet}
-              maxDistance={20}
-              minDistance={5}
-              minPolarAngle={Math.PI / 5}
-              maxPolarAngle={Math.PI / 2}
-            />
-            
-            <mesh>
-              <boxGeometry args={[1, 1, 1]} />
-              <meshStandardMaterial color='teal' />
-            </mesh>
-          </Canvas>
+          <HeroExperience />
         </figure>
       </div>
     </section>
