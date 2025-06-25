bfdd17b435833338624676c317614ca4bd2fe2ed
Wayne <waynem567@gmail.com>
2025-06-25 16:55:41 +0200

    Backtrack to understand better

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,33 +1,8 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { useMediaQuery } from 'react-responsive';
-import { Room } from './Room';
-import HeroLights from './HeroLights';
+import React from 'react'
 
 const HeroExperience = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
-  const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
-
   return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <OrbitControls 
-        enablePan={false}
-        enableZoom={!isTablet}
-        maxDistance={20}
-        minDistance={5}
-        minPolarAngle={Math.PI / 5}
-        maxPolarAngle={Math.PI / 2}
-      />
-      
-      <HeroLights />
-      <group
-       scale={isMobile ? 0.7 : 1}
-       position={[0, -3.5, 0]}
-       rotation={[0, -Math.PI / 4, 0]}
-      >
-        <Room />
-      </group>
-    </Canvas>
+    <div>HeroExperience</div>
   )
 }
 
--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -4,48 +4,48 @@ import * as THREE from 'three'
 const HeroLights = () => {
   return (
     <>
-      <spotLight 
+      {/* <spotLight 
        position={[2, 5, 6]}
-       angle={0.2}
+       angle={0.15}
        intensity={100} 
        penumbra={0.2}
        color='#ffffff'
-      />
+      /> */}
       
-      <spotLight 
+      {/* <spotLight 
        position={[4, 5, 4]}
        angle={0.3}
        intensity={40} 
        penumbra={0.5}
        color='#4cc9f0'
-      />
+      /> */}
       
-      <spotLight 
+      {/* <spotLight 
        position={[-3, 5, 5]}
        angle={0.4}
-       intensity={60} 
+       intensity={40} 
        penumbra={1}
        color='#9d4edd'
-      />
+      /> */}
 
-      <primitive
+      {/* <primitive
        object={new THREE.RectAreaLight('#a259ff', 8, 3, 2)}
        position={[1, 3, 4]}
        intensity={15}
        rotation={[-Math.PI / 4, Math.PI / 4, 0]}
-      />
+      /> */}
 
-      <pointLight 
+      {/* <pointLight 
        position={[0, 1, 0]}
        intensity={10}
        color={'#7209b7'}
-      />
+      /> */}
       
-      <pointLight 
+      {/* <pointLight 
        position={[1, 2, -2]}
        intensity={10}
        color={'#0d0084'}
-      />
+      /> */}
     </>
   )
 }
