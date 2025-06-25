95839d4f2b09e1ebe2278bc4c97e726a5e924a10
Wayne <waynem567@gmail.com>
2025-06-25 15:26:31 +0200

    Desktop lamp light

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const HeroLights = () => {
   return (
     <>
-      <ambientLight intensity={0.2} color='#1A1A40' />
-      <directionalLight position={[5, 5, 5]} intensity={2} />
+      <spotLight 
+       position={[2, 5, 6]}
+       angle={0.15}
+       intensity={100} 
+       penumbra={0.2}
+       color='white'
+      />
     </>
   )
 }
