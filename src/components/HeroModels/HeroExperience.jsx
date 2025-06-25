f209ac27c38acc0da1072f1566ee42b772d7b8fa
Wayne <waynem567@gmail.com>
2025-06-25 15:11:02 +0200

    Adjust position and rotation of Hero 3D model

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -23,6 +23,8 @@ const HeroExperience = () => {
       
       <group
        scale={isMobile ? 0.7 : 1}
+       position={[0, -3.5, 0]}
+       rotation={[0, -Math.PI / 4, 0]}
       >
         <Room />
       </group>
