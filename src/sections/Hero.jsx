b818bd60057de14c05117e9644c717911cedf5f4
Wayne <waynem567@gmail.com>
2025-06-25 14:12:56 +0200

    Retrace steps to solve Hero alignment issue

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import Button from '../components/Button.jsx'
 import HeroExperience from '../components/HeroModels/HeroExperience.jsx'
-import { words } from '../constants/index.js'
+// import { words } from '../constants/index.js'
 
 const Hero = () => {
   return (
@@ -15,35 +15,10 @@ const Hero = () => {
         <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
           <div className='flex flex-col gap-7'>
             <div className='hero-text'>
-                <h1>
-                  Shaping
-                  <span className='slide'>
-                    <span className='wrapper'>
-                      {words.map((word) => (
-                        <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
-                          <img 
-                            src={word.imgPath}
-                            alt={word.text} 
-                            className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
-                          />
-
-                          <span>{word.text}</span>
-                        </span>
-                      ))}
-                    </span>
-                  </span>
-                </h1>
-                <h1>into Real Projects</h1>
-                <h1>that Deliver Results</h1>
+              <h1>Shaping</h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1> 
             </div>
-            <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>
-              Hi, I'm Wayne, a developer based in South Africa with a passion for code.
-            </p>
-            <Button 
-              className='md:w-80 md:h-16 w-60 h-12'
-              id='button'
-              text='See my Work'
-            />
           </div>
         </header>
 
