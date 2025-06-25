8905e8269d47e7ab11ec0758a89ee2ad51153c66
Wayne <waynem567@gmail.com>
2025-06-25 15:54:18 +0200

    Reposition rectangular light

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -30,7 +30,7 @@ const HeroLights = () => {
 
       <primitive
        object={new THREE.RectAreaLight('#a259ff', 8, 3, 2)}
-       position={[0, 1, 0]}
+       position={[1, 3, 4]}
        intensity={15}
        rotation={[-Math.PI / 4, Math.PI / 4, 0]}
       />
