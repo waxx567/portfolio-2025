17c3c627f02b6570e24896a5672fb1bb7e66e386
Wayne <waynem567@gmail.com>
2025-06-26 09:43:07 +0200

    Add item labels

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -5,10 +5,11 @@ const AnimatedCounter = () => {
     <div id='counter' className='padding-x-lg xl:mt-0 mt-32'>
       <div className='mx-auto grid-4-cols'>
         {counterItems.map((item) => (
-          <div className='bg-zinc-900 rounded-lg p-10 flex flex-col justify-center'>
+          <div className='bg-zinc-900 rounded-lg p-10 flex flex-col justify-center'> {/* counter card */}
             <div key={counterItems.label} className='counter-number text-white text-5xl font-bold mb-2'>
                 0 {item.suffix}
-            </div>            
+            </div>   
+            <div>{item.label}</div>         
           </div>
         ))}
       </div>
