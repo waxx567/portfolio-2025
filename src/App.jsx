a7afe1f9abc929272aa6472b42ba0a841d3df73b
Wayne <waynem567@gmail.com>
2025-06-26 10:27:55 +0200

    Import and add ShowcaseSection to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from './sections/Hero.jsx'
+import ShowcaseSection from './sections/ShowcaseSection.jsx'
 
 const App = () => {
   return (
     <>
       <Hero />
+      <ShowcaseSection />
     </>
   )
 }
