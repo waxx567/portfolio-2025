7baad46e96947f729df3b54b1d2490f882743d7f
Wayne <waynem567@gmail.com>
2025-06-26 17:56:11 +0200

    Style and add first Showcase image

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const ShowcaseSection = () => {
   return (
-    <div>ShowcaseSection</div>
+    <div id='work'className='app-showcase'>
+      <div className='w-full'>
+        <div className='showcaselayout'>
+          {/* LEFT */}
+          <div className='first-project-wrapper'>
+            <div className='image-wrapper'>
+              <img src='./images/project-1.png' alt='project-1' />
+            </div>
+          </div>
+          {/* RIGHT */}
+        </div>
+      </div>
+    </div>
   )
 }
 
