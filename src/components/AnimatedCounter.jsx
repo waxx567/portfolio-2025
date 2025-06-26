1e41bb234dcf89dfecf58fcb13551060317e7e41
Wayne <waynem567@gmail.com>
2025-06-26 09:39:16 +0200

    Add counters

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { counterItems } from '../constants'
 
 const AnimatedCounter = () => {
   return (
-    <div>AnimatedCounter</div>
+    <div id='counter' className='padding-x-lg xl:mt-0 mt-32'>
+      <div className='mx-auto grid-4-cols'>
+        {counterItems.map((item) => (
+          <div className='bg-zinc-900 rounded-lg p-10 flex flex-col justify-center'>
+            <div key={counterItems.label} className='counter-number text-white text-5xl font-bold mb-2'>
+                0 {item.suffix}
+            </div>            
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
