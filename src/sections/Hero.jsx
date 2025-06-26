ddf776b6f6a3f6f9749a7f613f5c1cef0552038a
Wayne <waynem567@gmail.com>
2025-06-26 08:53:01 +0200

    GSAP for Hero h1 element animations

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 import Button from '../components/Button.jsx'
 import HeroExperience from '../components/HeroModels/HeroExperience.jsx'
 import { words } from '../constants/index.js'
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+    );
+  });
+  
   return (
     <section id='hero' className='relative overflow-hidden'>
       <div className='absolute top-0 left-0 z-10'>
