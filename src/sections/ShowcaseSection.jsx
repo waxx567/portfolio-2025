3272a2370248ffa347625c99e9c17e6bf7fe6313
Wayne <waynem567@gmail.com>
2025-06-27 19:36:23 +0200

    Animate projects with useGSAP hook

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -11,9 +11,29 @@ const ShowcaseSection = () => {
   const project2Ref = useRef(null);
   const project3Ref = useRef(null);
 
-  const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
-  
   useGSAP(() => {
+    const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
+
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+        y: 50, opacity: 0 
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=100px",
+            end: "bottom 20%"
+          }
+        }
+      )
+    })
+    
     gsap.fromTo(
      sectionRef.current,
      { opacity: 0 }, 
