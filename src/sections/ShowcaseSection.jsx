382dcb8fede8d5d144701ba223a62bb367e0fd87
Wayne <waynem567@gmail.com>
2025-06-27 08:51:18 +0200

    Import useGSAP hook

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react'
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';  
 
 gsap.registerPlugin(ScrollTrigger);
 
