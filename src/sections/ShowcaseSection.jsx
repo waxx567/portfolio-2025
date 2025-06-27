b176b88c816b4715cfb289b47d34d6b13a4cbf6c
Wayne <waynem567@gmail.com>
2025-06-27 08:43:56 +0200

    Add projectRefs to projects

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -11,7 +11,7 @@ const ShowcaseSection = () => {
       <div className='w-full'>
         <div className='showcaselayout'>
           {/* LEFT */}
-          <div className='first-project-wrapper'>
+          <div className='first-project-wrapper' ref={project1Ref}>
             <div className='image-wrapper'>
               <img src='../images/project1.png' alt='project 1' />
             </div>
@@ -25,14 +25,14 @@ const ShowcaseSection = () => {
 
           {/* RIGHT */}
           <div className='project-list-wrapper overflow-hidden'>
-            <div className='project'>
+            <div className='project' ref={project2Ref}>
               <div className='image-wrapper bg-[#ffefdb]'>
                 <img src='../images/project2.png' alt='project 2' />
               </div>
               <h2>Library Management Platform</h2>
             </div>
             
-            <div className='project'>
+            <div className='project' ref={project3Ref}>
               <div className='image-wrapper bg-[#ffe7eb]'>
                 <img src='../images/project3.png' alt='project 3' />
               </div>
