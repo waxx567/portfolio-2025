5bcabec0bc57f61f250a5ac2fc002ef9c43eea36
Wayne <waynem567@gmail.com>
2025-06-28 09:34:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NavBar from './components/NavBar.jsx'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowcaseSection />
     </>
