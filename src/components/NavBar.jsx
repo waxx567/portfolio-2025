05fd25987d674b076721c2d74b520ade79074316
Wayne <waynem567@gmail.com>
2025-06-28 11:38:44 +0200

    Add nav cta

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,14 @@ const NavBar = () => {
         <a className='logo' href='#hero'>
           Wayne | fivefiftyfive
         </a>
+
+        <nav className='desktop'></nav>
+
+        <a href='#contact' className='contact-btn group'>
+          <div className='inner'>
+            <span>Contact Me</span>
+          </div>
+        </a>
       </div>
     </header>
   )
