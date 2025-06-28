579f6b4ad30339b00caffb84abb44fd66e18f23b
Wayne <waynem567@gmail.com>
2025-06-28 18:48:21 +0200

    Update Hero & NavBar

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import gsap from "gsap";
 import AnimatedCounter from "../components/AnimatedCounter";
 import Button from "../components/Button";
 import { words } from "../constants";
-import HeroExperience from "../components/models/hero_models/HeroExperience";
+import HeroExperience from "../components/HeroModels/HeroExperience";
 
 const Hero = () => {
   useGSAP(() => {
