9cba79e7fbdb343dffcf2fb9b12c8a933bb5d9f6
Wayne <waynem567@gmail.com>
2025-06-28 18:40:25 +0200

    navLinks events

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,55 @@
-import { navLinks } from '../constants/index.js'
+import { useState, useEffect } from "react";
+
+import { navLinks } from "../constants";
 
 const NavBar = () => {
+  // track if the user has scrolled down the page
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    // create an event listener for when the user scrolls
+    const handleScroll = () => {
+      // check if the user has scrolled down at least 10px
+      // if so, set the state to true
+      const isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    // add the event listener to the window
+    window.addEventListener("scroll", handleScroll);
+
+    // cleanup the event listener when the component is unmounted
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className='navbar'>
-      <div className='inner'>
-        <a className='logo' href='#hero'>
-          Wayne | fivefiftyfive
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+      <div className="inner">
+        <a href="#hero" className="logo">
+          Adrian JSM
         </a>
 
-        <nav className='desktop'>
+        <nav className="desktop">
           <ul>
-            <li>
-              {navLinks.map(({ link, name }) => (
-                <li key={name} className='group'>
-                  <a href={link}>
-                    <span>{name}</span>
-                    <span className='underline' />
-                  </a>
-                </li>
-              ))}
-            </li>
+            {navLinks.map(({ link, name }) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
           </ul>
         </nav>
 
-        <a href='#contact' className='contact-btn group'>
-          <div className='inner'>
-            <span>Contact Me</span>
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact me</span>
           </div>
         </a>
       </div>
     </header>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
