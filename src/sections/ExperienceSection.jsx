3e3d23dc9e9ab56ac5ce7a6278a60cc4f7c0d2b5
Wayne <waynem567@gmail.com>
2025-06-29 17:30:28 +0200

    Improve legibility

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -4,7 +4,10 @@ const ExperienceSection = () => {
   return (
     <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
       <div className='w-full h-full md:px-20 px-5'>
-        <TitleHeader title='Professional Work Experience' sub='💼 My Career Overview' />
+        <TitleHeader
+         title='Professional Work Experience' 
+         sub='💼 My Career Overview' 
+        />
       </div>
     </section>
   )
