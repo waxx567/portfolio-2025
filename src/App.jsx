6152762a7d2de74610819c5a7048cffbc214cf57
Wayne <waynem567@gmail.com>
2025-06-29 10:15:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import LogoSection from './components/LogoSection.jsx'
 import NavBar from './components/NavBar.jsx'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
@@ -8,6 +9,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
     </>
   )
 }
