c87d633d5fc22a054fe043ceddfe1cef77e0fa5a
Wayne <waynem567@gmail.com>
2025-06-29 19:25:17 +0200

    Style stars

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -4,7 +4,7 @@ const GlowCard = ({ card, children }) => {
   return (
     <div className='card card-border timeline-card rounded-xl p-10'>
       <div className='glow' />
-      <div>
+      <div className='flex items-center gap-1 mb-5'>
         {Array.from({ length: 5}, (_, i) => (
           <img src='./images/star.png' key={i} alt='star' className='size-5' />
         ))}
