1514b5ade8e53995ca881c719060dea1d9093752
Wayne <waynem567@gmail.com>
2025-06-29 10:20:47 +0200

    Establish LogoSection layout

--- src/components/LogoSection.jsx
+++ src/components/LogoSection.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const LogoSection = () => {
   return (
-    <div>LogoSection</div>
+    <div className='md:my-20 my-10 relative'>
+      <div className='gradient-edge' />
+      <div className='gradient-edge' />
+    </div>
   )
 }
 
