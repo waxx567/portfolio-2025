39a2ec15f3d35b35fd757c7799aa28e781a02f5a
Wayne <waynem567@gmail.com>
2025-06-29 19:24:08 +0200

    Update GlowCard.jsx

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const GlowCard = () => {
+const GlowCard = ({ card, children }) => {
   return (
-    <div>GlowCard</div>
+    <div className='card card-border timeline-card rounded-xl p-10'>
+      <div className='glow' />
+      <div>
+        {Array.from({ length: 5}, (_, i) => (
+          <img src='./images/star.png' key={i} alt='star' className='size-5' />
+        ))}
+      </div>
+    </div>
   )
 }
 
