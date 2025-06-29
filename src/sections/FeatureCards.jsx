f95e83ab39b67207a7bbf815cd1d9ed59ad5eeef
Wayne <waynem567@gmail.com>
2025-06-29 13:08:48 +0200

    Render icons

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -5,7 +5,10 @@ const FeatureCards = () => {
     <div className='w-full padding-x-lg'>
       <div className='mx-auto grid-3-cols'>
         {abilities.map(({ imgPath, title, desc }) => (
-          <div>
+          <div key={title} className='card-border rounded-xl p-8 flex flex-col gap-4'>
+            <div className='size-14 flex items-center justify-center rounded-full'>
+              <img src={imgPath} alt={title} />
+            </div>
             <h3>{title}</h3>
           </div>
         ))}
