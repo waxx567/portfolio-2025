04287937f064d10ea43fa22a23d13cf77c974119
Wayne <waynem567@gmail.com>
2025-06-30 08:28:23 +0200

    Set up animation of card border

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const GlowCard = ({ card, children }) => {
+const GlowCard = ({ card, children, index }) => {
+  const cardRefs = useRef([]);
+
+  const handleMouseMove = (index) => (e) => {
+    const card = cardRefs.current[index];
+    if (!card) return;
+  }
+
   return (
-    <div className='card card-border timeline-card rounded-xl p-10'>
+    <div ref={(el) => (cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)} className='card card-border timeline-card rounded-xl p-10'>
       <div className='glow' />
       <div className='flex items-center gap-1 mb-5'>
         {Array.from({ length: 5}, (_, i) => (
