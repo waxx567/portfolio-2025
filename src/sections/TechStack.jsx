8cd80d93374e5ec2f45330ea0c2a4ebbf56750b1
Wayne <waynem567@gmail.com>
2025-06-30 17:20:40 +0200

    TitleHeader for TechStack section

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import TitleHeader from '../components/TitleHeader'
 
 const TechStack = () => {
   return (
-    <div>TechStack</div>
+    <div id='skills' className='flex-center section-padding'>
+      <div>
+        <TitleHeader
+         title='My Preferred Tech Stack' 
+         sub='🤝 The Skills I Brag About' 
+        />
+      </div>
+    </div>
   )
 }
 
