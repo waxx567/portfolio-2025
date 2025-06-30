8051c941955529ddfc6e92b2c710a9a211571c68
Wayne <waynem567@gmail.com>
2025-06-30 08:36:34 +0200

    Animate card border

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -6,6 +6,19 @@ const GlowCard = ({ card, children, index }) => {
   const handleMouseMove = (index) => (e) => {
     const card = cardRefs.current[index];
     if (!card) return;
+
+    // Calculate the position of the mouse relative to the card
+    const rect = card.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left - rect.width / 2;
+    const mouseY = e.clientY - rect.top - rect.height / 2;
+
+    // Calculate the angle of the mouse relative to the center of the card
+    let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+
+    angle = (angle + 360) % 360;
+
+    // Apply the rotation to the card
+    card.style.setProperty('--start', angle + 60);
   }
 
   return (
