6ec7c425e5cc25b33d4871f40b350639fbfe8aff
Wayne <waynem567@gmail.com>
2025-06-30 10:11:35 +0200

    Add responsibilities

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -36,8 +36,18 @@ const ExperienceSection = () => {
                       <div>
                         <h1 className='font-semibold text-3xl'>{card.title}</h1>
                         <p className='my-5 text-white-50'>
-                          {card.date}
+                          📅 {card.date}
                         </p>
+                        <p className='text-[#839cb5] italic'>
+                          Responsibilities
+                        </p>
+                        <ul className='list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50'>
+                          {card.responsibilities.map((responsibility) => (
+                            <li key={responsibility} className='text-lg'>
+                              {responsibility}
+                            </li>
+                          ))}
+                        </ul>
                       </div>
                     </div>
                   </div>
