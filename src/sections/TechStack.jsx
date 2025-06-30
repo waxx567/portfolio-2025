23caeab7b6115e5ce38f090a6a2da59f278fca87
Wayne <waynem567@gmail.com>
2025-06-30 19:01:12 +0200

    Initialize tech stack grid

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
+import { techStackIcons } from '../constants'
 
 const TechStack = () => {
   return (
     <div id='skills' className='flex-center section-padding'>
-      <div>
+      <div className='w-full h-full md:px-10 px-5'>
         <TitleHeader
          title='My Preferred Tech Stack' 
          sub='🤝 The Skills I Acquired'
         />
+
+        <div className='tech-grid'>
+          {techStackIcons.map((icon) => (
+            <div key={icon.name}>
+              {icon.name}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
