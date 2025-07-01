e9623a7c0ef45140cd46a4a63e305ed391ef135f
Wayne <waynem567@gmail.com>
2025-07-01 19:25:40 +0200

    Add OrbitControls

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -5,7 +5,7 @@
 // - Optionally tweaks the material (like setting a white colour on a specific mesh)
 // - Disables zoom using OrbitControls
 
-import { Environment, Float, useGLTF } from '@react-three/drei';
+import { Environment, Float, OrbitControls, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 const TechIcon = ({ model }) => {
@@ -15,7 +15,10 @@ const TechIcon = ({ model }) => {
     <Canvas>
       <ambientLight intensity={0.3} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
+
       <Environment preset='city' />
+
+      <OrbitControls enableZoom={false} />
 
       <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
         <group scale={model.scale} rotation={model.rotation}>
