9c48d4deaa52ae2469e057a5a6d337d39a933b20
Wayne <waynem567@gmail.com>
2025-07-01 13:55:26 +0200

    Rewind to fix bug

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,31 +1,8 @@
 import React from 'react'
-import TitleHeader from '../components/TitleHeader'
-import { techStackIcons } from '../constants'
-import TechIcon from '../components/Models/TechLogos/TechIcon'
 
 const TechStack = () => {
   return (
-    <div id='skills' className='flex-center section-padding'>
-      <div className='w-full h-full md:px-10 px-5'>
-        <TitleHeader
-         title='My Preferred Tech Stack' 
-         sub='🤝 The Skills I Acquired'
-        />
-
-        <div className='tech-grid'>
-          {techStackIcons.map((icon) => (
-            <div key={icon.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
-              <div className='tech-card-animated-bg' />
-              <div className='tech-card-content'>
-                <div className='tech-icon-wrapper'>
-                  <TechIcon model={icon} />
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    <div>TechStack</div>
   )
 }
 
