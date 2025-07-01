9ddfa794f754a7dcd6e63c7fb509b637983923f1
Wayne <waynem567@gmail.com>
2025-07-01 20:03:16 +0200

    Tech icon animations

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { techStackIcons } from '../constants'
 import TechIcon from '../components/Models/TechLogos/TechIcon'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const TechStack = () => {
+  useGSAP(() => {
+    gsap.fromTo(".tech-card", {
+      y: 50,
+      opacity: 0,
+    }, {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      ease: "power2.inOut",
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: "#skills",
+        start: "top center",
+      },
+    });
+  });
+
   return (
     <div id='skills' className='flex-center section-padding'>
       <div className='w-full h-full md:px-10 px-5'>
