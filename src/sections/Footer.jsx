7a2c120fe143a2b2bf959ba910b8ccc3606ea99f
Wayne <waynem567@gmail.com>
2025-07-02 19:55:38 +0200

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { socialImgs } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="flex flex-col justify-center">
+          <p><strong>FIVE50FIVE (PTY) LTD</strong></p>
+        </div>
+        <div className="socials">
+          {socialImgs.map((socialImg, index) => (
+            <div key={index} className="icon">
+              <img src={socialImg.imgPath} alt="social icon" />
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col justify-center">
+          <p className="text-center md:text-end">
+            © {new Date().getFullYear()} Wayne McRae. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
