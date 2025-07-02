c3cde15ccc89706dc12bacaf1d712c93fe53884c
Wayne <waynem567@gmail.com>
2025-07-02 11:18:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LogoSection from './sections/LogoSection.jsx'
 import FeatureCards from './sections/FeatureCards.jsx'
 import ExperienceSection from './sections/ExperienceSection.jsx'
 import TechStack from './sections/TechStack.jsx'
+import Testimonials from './sections/Testimonials.jsx'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <FeatureCards />
       <ExperienceSection />
       <TechStack />
+      <Testimonials />
     </>
   )
 }
