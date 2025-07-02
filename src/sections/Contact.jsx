b70b13d315c6248fe5b0c1a507bb8833860f4f67
Wayne <waynem567@gmail.com>
2025-07-02 15:19:56 +0200

    Update TitleHeader

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -43,8 +43,8 @@ const Contact = () => {
     <section id="contact" className="flex-center section-padding">
       <div className="w-full h-full md:px-10 px-5">
         <TitleHeader
-          title="Get in Touch – Let’s Connect"
-          sub="💬 Have questions or ideas? Let’s talk! 🚀"
+          title="Let’s Connect"
+          sub="💬 Get in Touch!" 
         />
         <div className="grid-12-cols mt-16">
           <div className="xl:col-span-5">
