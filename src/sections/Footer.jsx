fe959d07edf22966175b777fb267a21117339415
Wayne <waynem567@gmail.com>
2025-08-10 17:57:47 +0200

    Comments and copy change

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,11 +1,19 @@
 import { socialImgs } from "../constants";
 
+/**
+ * Footer component, which renders a simple footer with a link to my blog,
+ * my social media links, and a copyright notice.
+ *
+ * @returns {JSX.Element} The rendered footer element.
+ */
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         <div className="flex flex-col justify-center md:items-start items-center">
-          <a href="https://fivefiftyfive.io">Visit my blog</a>
+          More at <a href="https://fivefiftyfive.io">
+          <span className="bg-gradient-to-r from-purple-500 to-purple-800 text-transparent bg-clip-text">fivefiftyfive.io</span>
+</a>
         </div>
         <div className="socials">
           {socialImgs.map((socialImg, index) => (
