2880aa1235561b96ed6e350caaa29d76b6a0619d
Wayne <waynem567@gmail.com>
2025-08-11 00:24:39 +0200

    Style icons

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -13,7 +13,7 @@ const FeatureCards = () => {
         {abilities.map(({ imgPath, title, desc }) => (
           <div key={title} className='card-border rounded-xl p-8 flex flex-col gap-4'>
             <div className='size-14 flex items-center justify-center rounded-full'>
-              <img src={imgPath} alt={title} />
+              <img src={imgPath} alt={title} className='w-10 h-10' />
             </div>
             <h3 className='text-white text-2xl font-semibold mt-2'>{title}</h3>
             <p className='text-white-50 text-lg'>{desc}</p>
