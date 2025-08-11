363b8296502c6706e9122e77cfed5776677ec733
Wayne <waynem567@gmail.com>
2025-08-11 11:57:34 +0200

    Hide testimonials for now

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import LogoSection from './sections/LogoSection.jsx'
 import FeatureCards from './sections/FeatureCards.jsx'
 import ExperienceSection from './sections/ExperienceSection.jsx'
 import TechStack from './sections/TechStack.jsx'
-import Testimonials from './sections/Testimonials.jsx'
+// import Testimonials from './sections/Testimonials.jsx'
 import Contact from './sections/Contact.jsx'
 import Footer from './sections/Footer.jsx'
 
@@ -27,7 +27,7 @@ const App = () => {
       <FeatureCards />
       <ExperienceSection />
       <TechStack />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
